f2b5f202c886db4ea0199a3bfa5461c471ce2708
andresdotfelipe <andresdotfelipecs@outlook.com>
2022-06-24 23:21:28 -0500

    add Landing Alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import 'emerald-ui/lib/styles.css';
 import Header from './components/Header';
+import LandingAlert from './components/LandingAlert';
 
 const App = () => {
   return (
     <>      
       <Header />
+      <main className="eui-container">
+        <LandingAlert />
+      </main>      
     </>
   );
 }
---
+++ src/components/LandingAlert/LandingAlert.jsx
@@ -0,0 +1,16 @@
+import './LandingAlert.scss';
+import { useState } from 'react';
+import Alert from 'emerald-ui/lib/Alert';
+
+const LandingAlert = () => {
+    const [showAlert, setShowAlert] = useState(true);
+
+    return (
+        showAlert &&
+        <Alert onDismiss={() => setShowAlert(false)}>
+            <p>Welcome to the new look of News.com. Keep scrolling to discover interesting new features and news.</p>            
+        </Alert>
+    );
+}
+
+export default LandingAlert;
